433efaac94cb7b401a7de13e195379bc94fcb3e7
eden-owo <edenwu0625@gmail.com>
2025-05-23 13:49:49 +0800

    Modulize main.cpp

--- ocr_module.h
+++ ocr_module.h
@@ -1,1 +1,10 @@
 #pragma once
+#include <Windows.h>
+#include <opencv2/opencv.hpp>
+
+typedef struct {
+    __int32 t, col, row, _unk;
+    __int64 step, data_ptr;
+} Img;
+
+void run_ocr(const Img& img);
