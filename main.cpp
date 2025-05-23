6035a1aaaa93ba4ff594fe553c82651ac25e83aa
eden-owo <edenwu0625@gmail.com>
2025-05-23 16:25:02 +0800

    Update imgs folder reading.

--- main.cpp
+++ main.cpp
@@ -1,43 +1,57 @@
 #include <opencv2/opencv.hpp>
 #include <iostream>
 #include "ocr_module.h"
+#include "folder.h"
 
 using namespace std;
 using namespace cv;
 
 int main(int argc, char* argv[]) {
-    if (argc < 2) {
-        cout << "Usage: ocr.exe <image.png>\n";
-        return 0;
-    }
 
-    Mat img = imread(argv[1], IMREAD_UNCHANGED);
-    if (img.empty()) {
-        cerr << "Cannot read image.\n";
-        return -1;
-    }
+    cout << "Please select img folder." << endl;
+    std::string img_folder = ReadFolder();
+    cout << "Please select output folder." << endl;
+    std::string output_folder = ReadFolder();
 
-    Mat img_rgba;
-    if (img.channels() == 3) {
-        cvtColor(img, img_rgba, COLOR_BGR2BGRA);
-    }
-    else if (img.channels() == 4) {
-        img_rgba = img;
-    }
-    else {
-        cerr << "Unsupported image format.\n";
-        return -1;
-    }
+    std::vector<std::string> imageFiles = ListFilesInFolder(img_folder);
 
-    Img ig = {
-        3,
-        img_rgba.cols,
-        img_rgba.rows,
-        0,
-        (__int64)img_rgba.step,
-        (__int64)img_rgba.data
-    };
+    //if (argc < 2) {
+    //    cout << "Usage: ocr.exe <image.png>\n";
+    //    return 0;
+    //}
 
-    run_ocr(ig);
+    for (const auto& filePath : imageFiles) {
+        std::cout << "Processing: " << filePath << std::endl;
+
+        Mat img = imread(argv[1], IMREAD_UNCHANGED);
+        if (img.empty()) {
+            cerr << "Cannot read image.\n";
+            return -1;
+        }
+
+        Mat img_rgba;
+        if (img.channels() == 3) {
+            cvtColor(img, img_rgba, COLOR_BGR2BGRA);
+        }
+        else if (img.channels() == 4) {
+            img_rgba = img;
+        }
+        else {
+            cerr << "Unsupported image format.\n";
+            return -1;
+        }
+
+        Img ig = {
+            3,
+            img_rgba.cols,
+            img_rgba.rows,
+            0,
+            (__int64)img_rgba.step,
+            (__int64)img_rgba.data
+        };
+
+        run_ocr(ig);
+    }
+    std::cout << "No images remaining. Processing finished." << std::endl;
     return 0;
 }
